a17a13dd4f0571da30d0db35e5c9d28a644f7293
danng2000 <dan.ng.174@gmail.com>
2026-02-25 20:01:17 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/valentine-gift-app/', // GitHub Pages repo name — change if repo name differs
-})
